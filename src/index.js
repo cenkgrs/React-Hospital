d0bd6008a34b539bf2b46d4938f09a08abe0c746
cenk Gürses <cenkgrs@gmail.com>
2019-12-24 11:39:49 +0200

    slidebar added

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Nav from "./Nav";
+import Slidebar from "./Slidebar";
 
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Nav />, document.getElementById("navbar"));
+ReactDOM.render(<Slidebar />, document.getElementById("slidebar"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
